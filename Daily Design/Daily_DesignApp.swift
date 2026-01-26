91011516db4e4baefb259e680c50c1e2f2c30fc4
Rishabh@831 <rishabhbhati831@gmail.com>
2026-01-26 11:54:48 +0530

    Initial Commit

---
+++ Daily Design/Daily_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_DesignApp.swift
+//  Daily Design
+//
+//  Created by Rishabh Kumar on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Daily_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
